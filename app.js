de9a725e70c58168e4eb0abb933a269045bec41e
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2025-04-05 21:18:17 +0100

    handling cors
    Signed-off-by: Vibhishan Ranga <vibhishan.ranga1410@gmail.com>

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const Router = require("./routes");
 const app = express();
+const cors = require("cors");
 const { sequelize, Word } = require("./models");
 
 const PORT = 3000;
 
 async function init() {
+  // adding cors to allow requests from the frontend
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+      optionsSuccessStatus: 200,
+      maxAge: 3600,
+    })
+  );
+
   // we need to use express.json() to parse the body of the request
   app.use(express.json());
   app.use("/api", Router);
@@ -13,15 +26,15 @@ async function init() {
   // syncing the database -> this will create the tables if they don't exist
   await sequelize.sync();
 
-//   await Word.bulkCreate([
-//     { title: "apple" },
-//     { title: "banana" },
-//     { title: "cherry" },
-//     { title: "date" },
-//     { title: "fig" },
-//     { title: "grape" },
-//     { title: "kiwi" },
-//   ]);
+  //   await Word.bulkCreate([
+  //     { title: "apple" },
+  //     { title: "banana" },
+  //     { title: "cherry" },
+  //     { title: "date" },
+  //     { title: "fig" },
+  //     { title: "grape" },
+  //     { title: "kiwi" },
+  //   ]);
 
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
